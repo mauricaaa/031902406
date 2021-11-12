608ac63aa1c2101cddfa0b3b1be3168b4ba412b6
mauricaaa <90022245+mauricaaa@users.noreply.github.com>
2021-11-12 21:24:00 +0800

    Add files via upload

---
+++ home/home.js
@@ -0,0 +1,26 @@
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  data: {
+    elements: [{
+        title: '导入Excel',
+        name: 'shangchuan',
+        color: 'green',
+        icon: 'copy'
+      },
+      {
+        title: '开始点名',
+        name: 'dianmin',
+        color: 'orange',
+        icon: 'myfill'
+      } ,
+      {
+        title: '点名记录',
+        name: 'dmlist',
+        color: 'grey',
+        icon: 'roundcheckfill'
+      }
+    ],
+  }
+})
